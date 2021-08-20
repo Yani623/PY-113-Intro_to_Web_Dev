daff6d31895366c715178b8089716fc93dcb0405
YaniraHagstrom <yanirahagstrom@gmail.com>
2021-08-19 21:30:17 -0500

    Tic Tac Toe: Pieces added and turns limited

--- Day_15/TicTacToe/index.js
+++ Day_15/TicTacToe/index.js
@@ -3,76 +3,64 @@
 var x = "x.png"
 var o = "o.png"
 
-let clickCount = 0;
-console.log(clickCount);
-
+let begin = true;
+console.log(begin);
+const boxLi = []
 // Start button toggles from "Start Game" to "Restart" and resets clicks. 
 $(".begin").click(()=> {
     if (($(".begin").text()) == "Start Game"){
         $(".img-fluid").attr("src", "");
         $(".begin").text("Restart");
-        clickCount += 1;
-        console.log(clickCount);
+        begin = false;
+        console.log(begin);
         }
     else {
-        $(".img-fluid").attr("src", x);
+        $(".r1-c1").attr("src", x);
+        $(".r1-c2").attr("src", o);
+        $(".r1-c3").attr("src", x);
+        $(".r2-c1").attr("src", o);
+        $(".r2-c2").attr("src", x);
+        $(".r2-c3").attr("src", o);
+        $(".r3-c1").attr("src", x);
+        $(".r3-c2").attr("src", o);
+        $(".r3-c3").attr("src", x);
         $(".begin").text("Start Game");
-        clickCount = 0;
-        console.log(clickCount);
+        begin = true;
+        console.log(begin);
+        boxLI = []
         }
 });
 
-// $(".begin").click(()=> {
-//     if (clickCount == 0){
-//     $(".img-fluid").attr("src", x);
-//     $(".begin").text("Begin");
-//     clickCount += 1;
-//     console.log(clickCount)
-//     }
-// });
-
-
-
-
 // This identifies which square the player clicked on (Only after the game is started):
+var turn = 1;
+
 $(".grid").click((box)=>{
-    if (($(".begin").text()) == "Restart"){
+    if (begin == false && turn < 10){
         var id = box.target.id;
         console.log((id));
         var imgID = "." + id;
         var boxID = "#" + id;
         console.log(imgID);
-        console.log(boxID);
-
-        // Game Begins:
-        
+        console.log(boxID);    
+    // Game Begins:
+        // Determines if the box is already taken:
+        if (id.length > 2){ 
+            // 1st Players Turn:
+            if (turn % 2 != 0){ 
+                console.log(`This was turn ${turn}`)
+                $(imgID).attr("src", x);
+                turn++;
+                boxLi.push(imgID)
+                console.log(`Now its turn ${turn}`)
+            }
+                // 2nd Players Turn:
+            else if (turn % 2 == 0) {
+                console.log(`This was turn ${turn}`)
+                $(imgID).attr("src", o);
+                turn++;
+                boxLi.push(imgID,boxID)
+                console.log(`Now its turn ${turn}`)
+            }
+        }
     };
 });
-
-// var colID = "." + id1;
-// var imgID = "#" + id1;
-// console.log(colID);
-// console.log(imgID);
-
-// $(colID).click(() => {
-// $(imgID).attr("src", x);
-// clickCount += 1;
-// console.log(clickCount);
-// });
-
-
-
-// if (clickCount == 1){
-//     $(colID).click(() => {
-//     $(imgID).attr("src", x);
-//     clickCount += 1;
-//     console.log(clickCount);
-//     });
-// };
-
-// // Click to Begin Game:
-// if (clickCount == 1 and target.is()){
-//     $("#r1-c1").click(() => {
-//         $("#11").attr("src", o);
-//     });
-// };
